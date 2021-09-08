12e0ef7c3d98ffb37e02c7174324583e239b7bc8
AnnaDorosheva <annador@ukr.net>
2021-09-08 09:06:21 +0300

    change fetch key entry

--- src/js/api-news.js
+++ src/js/api-news.js
@@ -1,18 +1,19 @@
 const baseUrl = "https://newsapi.org/v2/everything";
+const key = "&apiKey=5586a16d91a8437d9271f83485961bc6";
 
 export default {
   page: 1,
   query: "",
   feachArticles() {
-    const options = {
-      headers: {
-        Authorization: "5586a16d91a8437d9271f83485961bc6",
-      },
-    };
+    // const options = {
+    //   headers: {
+    //     Authorization: "5586a16d91a8437d9271f83485961bc6",
+    //   },
+    // };
 
     const params = `?q=${this.query}&page=${this.page}`;
 
-    return fetch(baseUrl + params, options).then((response) => {
+    return fetch(baseUrl + params + key).then((response) => {
       this.inctementPage();
       return response.json();
     });
