ac38c70b8277833c6bc0bf0474a1e2d45c82a959
AnnaDorosheva <annador@ukr.net>
2021-09-14 10:24:42 +0300

    add header

--- src/js/api-news.js
+++ src/js/api-news.js
@@ -1,19 +1,18 @@
 const baseUrl = "https://newsapi.org/v2/everything?";
-const key = "apiKey=5586a16d91a8437d9271f83485961bc6";
+// const key = "apiKey=5586a16d91a8437d9271f83485961bc6";
 
 export default {
   page: 1,
   query: "",
   feachArticles() {
     const options = {
-      mode: "no-cors",
-      // headers: {
-      //   Authorization: "5586a16d91a8437d9271f83485961bc6",
-      // },
+      headers: {
+        Authorization: "5586a16d91a8437d9271f83485961bc6",
+      },
     };
-    const params = `&q=${this.query}&page=${this.page}&pageSize=3`;
+    const params = `q=${this.query}&page=${this.page}&pageSize=3`;
 
-    return fetch(baseUrl + key + params).then((response) => {
+    return fetch(baseUrl + params, options).then((response) => {
       this.inctementPage();
       return response.json();
     });
