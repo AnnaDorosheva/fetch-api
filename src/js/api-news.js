03aa474cb0fd22e9f58cf1f5a9bd209c9bed7e5a
AnnaDorosheva <annador@ukr.net>
2021-09-14 09:49:10 +0300

    fixed cors problem

--- src/js/api-news.js
+++ src/js/api-news.js
@@ -1,30 +1,25 @@
-// const baseUrl = "https://newsapi.org/v2/everything";
-const baseUrl =
-  "https://newsdata.io/api/1/news?apikey=pub_1165beaa4ce55554caca54070de07b96a86f&language=en";
-// const key = "&apiKey=5586a16d91a8437d9271f83485961bc6";
-
-// pub_1165beaa4ce55554caca54070de07b96a86f
+const baseUrl = "https://newsapi.org/v2/everything?";
+const key = "apiKey=5586a16d91a8437d9271f83485961bc6";
 
 export default {
   page: 1,
   query: "",
   feachArticles() {
-    // const options = {
-    //   headers: {
-    //     // Authorization: "5586a16d91a8437d9271f83485961bc6",
-    //     Accept: "application/json",
-    //   },
-    // };
+    const options = {
+      mode: "no-cors",
+      // headers: {
+      //   Authorization: "5586a16d91a8437d9271f83485961bc6",
+      // },
+    };
+    const params = `&q=${this.query}&page=${this.page}&pageSize=3`;
 
-    const params = `&q=${this.query}&page=${this.page}`;
-
-    return fetch(baseUrl + params).then((response) => {
+    return fetch(baseUrl + key + params).then((response) => {
       this.inctementPage();
       return response.json();
     });
   },
   searchQuery(text) {
-    this.query = text;
+    this.query = `${text}`;
   },
   inctementPage() {
     this.page += 1;
